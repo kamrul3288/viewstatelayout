18f5213ec98f9117e19dbe08c62621d8590cbd15
Kamrul Hasan <kamrulhasan3288@gmail.com>
2022-09-27 14:01:29 +0600

    release version 1.0.7

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -2,6 +2,6 @@ object AppConfig {
     const val minSdkVersion = 21
     const val compileSdkVersion = 33
     const val targetSdkVersion = 33
-    const val versionCode  = 6
-    const val versionName  = "1.0.6"
+    const val versionCode  = 7
+    const val versionName  = "1.0.7"
 }
