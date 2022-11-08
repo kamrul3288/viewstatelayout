c613b2e45e731966327ab1cf475e4fdd28f8736b
KamrulHasan <kamrulhasan3288@gmail.com>
2022-11-08 14:42:12 +0600

    release 1.0.8

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -2,6 +2,6 @@ object AppConfig {
     const val minSdkVersion = 21
     const val compileSdkVersion = 33
     const val targetSdkVersion = 33
-    const val versionCode  = 7
-    const val versionName  = "1.0.7"
+    const val versionCode  = 8
+    const val versionName  = "1.0.8"
 }
