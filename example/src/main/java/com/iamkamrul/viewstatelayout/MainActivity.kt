d86a092fd29d1a32759f2eb965c4e55079b53c79
KamrulHasan <kamrulhasan3288@gmail.com>
2022-11-27 23:11:33 +0600

    implement example

--- example/src/main/java/com/iamkamrul/viewstatelayout/MainActivity.kt
+++ example/src/main/java/com/iamkamrul/viewstatelayout/MainActivity.kt
@@ -2,6 +2,8 @@ package com.iamkamrul.viewstatelayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import com.iamkamrul.vsl.ViewStateLayoutConfig
 import com.iamkamrul.viewstatelayout.databinding.ActivityMainBinding
@@ -17,59 +19,68 @@ class MainActivity : AppCompatActivity() {
 
         //set global configuration
         ViewStateLayoutConfig.progressBarColor = R.color.purple_700
-        ViewStateLayoutConfig.networkErrorButtonDrawableRes = R.drawable.button_bg
+        ViewStateLayoutConfig.networkErrorButtonDrawableRes = R.drawable.exaple_vsl_button_bg
         ViewStateLayoutConfig.networkErrorButtonTextColor = R.color.black
-        ViewStateLayoutConfig.dataEmptyButtonBgDrawableRes = R.drawable.button_bg
+        ViewStateLayoutConfig.dataEmptyButtonBgDrawableRes = R.drawable.exaple_vsl_button_bg
         ViewStateLayoutConfig.dataEmptyButtonTextColor = R.color.black
         ViewStateLayoutConfig.networkErrorButtonStartDrawableRes = R.color.black
 
 
-        binding.progressBtn.elevation = 0f
+    }
 
-        binding.progressBtn.setOnClickListener {
-            binding.root.setSimpleProgressBarStatusLayout(true)
-        }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_vsl,menu)
+        return true
+    }
 
-        binding.networkError.setOnClickListener {
-            binding.root.showSimpleNetworkErrorLayout(
-                title = "Something went wrong",
-                message = "Check your network connection"
-            ) {
-                Toast.makeText(applicationContext, "refresh Call", Toast.LENGTH_SHORT).show()
-            }
-        }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.vslLoadingView -> binding.root.progressbarView(true)
 
-        binding.dataEmptyBtn.setOnClickListener {
-            binding.root.showSimpleDataEmptyLayout(
-                title = "Lorem Ipsum",
-                message = "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-            ) {
-                Toast.makeText(applicationContext, "data refresh Call", Toast.LENGTH_SHORT).show()
+            R.id.vslNetworkErrorView -> {
+                binding.root.networkErrorView(
+                    title = "Something went wrong",
+                    message = "Check your network connection",
+                    refreshCallback = {
+                        Toast.makeText(applicationContext, "refresh Call", Toast.LENGTH_SHORT).show()
+                    }
+                )
             }
 
-        }
+            R.id.vslDataEmptyErrorView -> {
+                binding.root.dataEmptyView(
+                    title = "Lorem Ipsum",
+                    message = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                    refreshCallback = {
+                        Toast.makeText(applicationContext, "data refresh Call", Toast.LENGTH_SHORT).show()
+                    }
+                )
 
-        binding.lottieProgressBtn.setOnClickListener {
-            binding.root.setLottieProgressBarStatusLayout(true)
-        }
-
-        binding.lottieNetworkError.setOnClickListener {
-            binding.root.showLottieNetworkErrorLayout(
-                title = "Something went wrong",
-                message = "Check your network connection"
-            ) {
-                Toast.makeText(applicationContext, "refresh Call", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        binding.lottieDataEmptyBtn.setOnClickListener {
-            binding.root .showLottieDataEmptyLayout(
-                title = "Lorem Ipsum",
-                message = "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-            ) {
-                Toast.makeText(applicationContext, "data refresh Call", Toast.LENGTH_SHORT).show()
             }
 
+            R.id.vslLoadingViewLottie -> binding.root.progressBarLottieView(true)
+
+            R.id.vslNetworkErrorViewLottie -> {
+                binding.root.networkErrorLottieView(
+                    title = "Something went wrong",
+                    message = "Check your network connection",
+                    refreshCallback = {
+                        Toast.makeText(applicationContext, "refresh Call", Toast.LENGTH_SHORT).show()
+                    }
+                )
+            }
+
+            R.id.vslDataEmptyErrorViewLottie -> {
+                binding.root .dataEmptyLottieView(
+                    title = "Lorem Ipsum",
+                    message = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                    refreshCallback = {
+                        Toast.makeText(applicationContext, "data refresh Call", Toast.LENGTH_SHORT).show()
+                    }
+                )
+
+            }
         }
+        return super.onOptionsItemSelected(item)
     }
 }
